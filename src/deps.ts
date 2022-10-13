85b87e34edd395996c48a78a1d7fe2ed529724b1
vwkd <33468089+vwkd@users.noreply.github.com>
2022-10-13 23:54:53 +0400

    src: reuse files from kita-server

---
+++ src/deps.ts
@@ -0,0 +1,2 @@
+export { serve } from "https://deno.land/std@0.154.0/http/server.ts";
+export { graphql, GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLID, GraphQLNonNull, GraphQLInt, GraphQLList, GraphQLEnumType, GraphQLUnionType, GraphQLBoolean, printSchema } from "https://cdn.skypack.dev/graphql@16.6.0";
