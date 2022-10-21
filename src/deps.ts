6e1f8668808335159e5c1b9b35d24a624ca06ac8
vwkd <33468089+vwkd@users.noreply.github.com>
2022-10-21 20:01:42 +0400

    schema: MVP, test data

--- src/deps.ts
+++ src/deps.ts
@@ -1,2 +1,23 @@
 export { serve } from "https://deno.land/std@0.154.0/http/server.ts";
 export { graphql, GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLID, GraphQLNonNull, GraphQLInt, GraphQLList, GraphQLEnumType, GraphQLUnionType, GraphQLBoolean, printSchema } from "https://cdn.skypack.dev/graphql@16.6.0";
+
+const export entries = [
+  {
+    id: 1,
+    value: {
+      PRS: {
+        S1: {
+          person1: {
+            value: "ვ",
+          },
+          root: {
+            value: "წერ",
+          },
+          person2: {
+            value: "თ",
+          },
+        }
+      }
+    }
+  },
+]
