bed0c65d250cbaead13117f8bcff8ab3baf1ec29
vwkd <33468089+vwkd@users.noreply.github.com>
2022-10-21 20:01:42 +0400

    schema: MVP, test data

--- src/deps.ts
+++ src/deps.ts
@@ -1,3 +1,24 @@
 export { serve } from "https://deno.land/std@0.154.0/http/server.ts";
 export { graphql, GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLID, GraphQLNonNull, GraphQLInt, GraphQLUnionType, printSchema } from "https://cdn.skypack.dev/graphql@16.6.0";
 export { log } from "https://raw.githubusercontent.com/vwkd/kita-utils/59bcf041c1e7ee6936423e787316dc65c2e5d157/src/logger.ts";
+
+export const entries = [
+  {
+    id: 1,
+    value: {
+      PRS: {
+        S1: {
+          person1: {
+            value: "ვ",
+          },
+          root: {
+            value: "წერ",
+          },
+          person2: {
+            value: "თ",
+          },
+        }
+      }
+    }
+  },
+]
---
+++ src/schema.ts
@@ -0,0 +1,112 @@
+import { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLID, GraphQLNonNull, GraphQLInt, GraphQLUnionType } from "./deps.ts";
+import { database } from "./database.ts"
+
+// --------- RESOLVER ---------
+
+function entryResolver(_, { id }) {
+
+  // TODO: assert ID is provided, is ID type
+  // TODO: error handling entry with id does not exist
+  
+  return database.entry(id);
+}
+
+// --------- SCHEMA ---------
+
+// BEWARE: definitions must be in order from leaf types all the way up to root type
+
+const person1Type = new GraphQLObjectType({
+  name: "Person1",
+  fields: {
+    value: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+  }
+});
+
+const rootType = new GraphQLObjectType({
+  name: "Root",
+  fields: {
+    value: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+  }
+});
+
+const person2Type = new GraphQLObjectType({
+  name: "Person2",
+  fields: {
+    value: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+  }
+});
+
+const subjectS1Type = new GraphQLObjectType({
+  name: "SubjectS1",
+  fields: {
+    // todo: add object persons, add other verb form components
+    person1: {
+      type: new GraphQLNonNull(person1Type),
+    },
+    root: {
+      type: new GraphQLNonNull(rootType),
+    },
+    person2: {
+      type: new GraphQLNonNull(person2Type),
+    },
+  }
+});
+
+const presentType = new GraphQLObjectType({
+  name: "PRS",
+  fields: {
+    // todo: add other subject persons
+    S1: {
+      type: new GraphQLNonNull(subjectS1Type),
+    },
+  }
+});
+
+
+const valueType = new GraphQLObjectType({
+  name: "Value",
+  fields: {
+    // todo: add series and groups, add other screeves
+    PRS: {
+      type: new GraphQLNonNull(presentType),
+    },
+  }
+});
+
+
+const entryType = new GraphQLObjectType({
+  name: "Entry",
+  fields: {
+    id: {
+      type: new GraphQLNonNull(GraphQLID),
+    },
+    value: {
+      type: new GraphQLNonNull(valueType),
+    },
+  }
+});
+
+const queryType = new GraphQLObjectType({
+  name: "Query",
+  fields: {
+    entry: {
+      type: entryType,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLID),
+        },
+      },
+      resolve: entryResolver,
+    },
+  },
+});
+
+export const schema = new GraphQLSchema({
+  query: queryType,
+});
