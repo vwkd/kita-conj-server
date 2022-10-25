7e26634db25b63f1c21ebaca16a42761b11f0e18
vwkd <33468089+vwkd@users.noreply.github.com>
2022-10-25 20:00:47 +0400

    schema: add series and groups

--- src/generate.ts
+++ src/generate.ts
@@ -15,53 +15,119 @@ function generate({ id, preverb, version, root, thema }) {
 }
 
 function getTable(args) {
+  const SRS1 = getSRS1(args);
+  const SRS2 = getSRS2(args);
+  const SRS3 = getSRS3(args);
+  const INF = getINF(args);
+  const IMP = getIMP(args);
+  
+  return {
+    SRS1,
+    SRS2,
+    SRS3,
+    IMP,
+    INF,
+  };
+}
+
+function getGRP1(args) {
   const PRS = getScreeve(args, getFormPresent);
   // todo: placeholder, fill with actual screeves
   const IMPF = PRS;
   const PRSSUBJ = PRS;
+  
+  return {
+    PRS,
+    IMPF,
+    PRSSUBJ,
+  };
+}
+
+function getGRP2(args) {
+  const PRS = getScreeve(args, getFormPresent);
+  // todo: placeholder, fill with actual screeves
   const FUT = PRS;
   const COND = PRS;
   const FUTSUBJ = PRS;
+
+  return {
+    FUT,
+    COND,
+    FUTSUBJ,
+  };
+}
+
+function getSRS1(args) {
+  const GRP1 = getGRP1(args);
+  const GRP2 = getGRP2(args);
+  
+  return {
+    GRP1,
+    GRP2,
+  };
+}
+
+function getSRS2(args) {
+  const PRS = getScreeve(args, getFormPresent);
+  // todo: placeholder, fill with actual screeves
   const AOR = PRS;
   const AORIMPF = PRS;
   const OPT = PRS;
   const OPTIMPF = PRS;
+  
+  return {
+    AOR,
+    AORIMPF,
+    OPT,
+    OPTIMPF,
+  };
+}
+
+function getSRS3(args) {
+  const PRS = getScreeve(args, getFormPresent);
+  // todo: placeholder, fill with actual screeves
   const PERF = PRS;
   const PERFIMPF = PRS;
   const PLUPERF = PRS;
   const PLUPERFIMPF = PRS;
   const PERFSUBJ = PRS;
   const PERFSUBJIMPF = PRS;
-  const IMPAFF = PRS;
-  const IMPAFFIMPF = PRS;
-  const IMPPRB1 = PRS;
-  const IMPPRB1IMPF = PRS;
-  const IMPPRB2 = PRS;
-  const INF = PRS;
-  const INFIMPF = PRS;
   
   return {
-    PRS,
-    IMPF,
-    PRSSUBJ,
-    FUT,
-    COND,
-    FUTSUBJ,
-    AOR,
-    AORIMPF,
-    OPT,
-    OPTIMPF,
     PERF,
     PERFIMPF,
     PLUPERF,
     PLUPERFIMPF,
     PERFSUBJ,
     PERFSUBJIMPF,
+  };
+}
+
+function getIMP(args) {
+  const PRS = getScreeve(args, getFormPresent);
+  // todo: placeholder, fill with actual screeves
+  const IMPAFF = PRS;
+  const IMPAFFIMPF = PRS;
+  const IMPPRB1 = PRS;
+  const IMPPRB1IMPF = PRS;
+  const IMPPRB2 = PRS;
+  
+  return {
     IMPAFF,
     IMPAFFIMPF,
     IMPPRB1,
     IMPPRB1IMPF,
     IMPPRB2,
+  };
+}
+
+function getINF(args) {
+  const PRS = getScreeve(args, getFormPresent);
+  // todo: placeholder, fill with actual screeves
+  const INF = PRS;
+  const INFIMPF = PRS;
+  
+  return {
     INF,
     INFIMPF,
   };
