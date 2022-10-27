515ce6cd2b7986cc80c742a445680b54afacf8e9
vwkd <33468089+vwkd@users.noreply.github.com>
2022-10-28 00:51:55 +0400

    form: refactor, only setters so can set multiple times, validation, helper getters

--- src/forms/present.ts
+++ src/forms/present.ts
@@ -1,69 +1,53 @@
-import { error, Form, validateArgs, validateExceptions } from "./utils.ts";
+import { error, Form } from "./utils.ts";
 
-function select_pz_s({ root, thema }, person_s_selected) {
-  const stem = thema ? root + thema : root;
-  
-  return person_s_selected == "S1"
-    ? {
-      person1: "ვ",
-      person2: null,
-    }
-    : person_s_selected == "S2"
-    ? {
-        person1: null,
-        person2: null,
-      }
-    : person_s_selected == "S3"
-    ? {
-        person1: null,
-        person2: "ს",
-      }
-    : person_s_selected == "P1"
-    ? {
-        person1: "ვ",
-        person2: "თ",
-      }
-    : person_s_selected == "P2"
-    ? {
-        person1: null,
-        person2: "თ",
-      }
-    : person_s_selected == "P3"
-    ? {
-        person1: null,
-        person2: stem.endsWith("ი") ? "ან" : "ენ",
-      }
-    : error(`Invalid person_s_selected "${person_s_selected}"`);
+function select_person1_s(stem, person_s) {
+  return person_s == "S1"
+    ? "ვ"
+    : person_s == "S2"
+    ? null
+    : person_s == "S3"
+    ? null
+    : person_s == "P1"
+    ? "ვ"
+    : person_s == "P2"
+    ? null
+    : person_s == "P3"
+    ? null
+    : error(`Invalid person_s "${person_s}"`);
 }
 
-export default function getPRS(args, exceptions, person_s_selected) {
-  validateArgs(args);
-  validateExceptions(exceptions);
+function select_person2_s(stem, person_s) {
+  return person_s == "S1"
+    ? null
+    : person_s == "S2"
+    ? null
+    : person_s == "S3"
+    ? "ს"
+    : person_s == "P1"
+    ? "თ"
+    : person_s == "P2"
+    ? "თ"
+    : person_s == "P3"
+    ? stem.endsWith("ი") ? "ან" : "ენ"
+    : error(`Invalid person_s "${person_s}"`);
+}
+
+export default function getPRS(args, exceptions, person_s) {
+  const form = Form();
   
-  const preverb = exceptions?.preverb?.value !== undefined ? exceptions.preverb.value : null;
-  const version = exceptions?.version?.value !== undefined ? exceptions.version.value : args.version;
-  const root = exceptions?.root?.value !== undefined ? exceptions.root.value : args.root;
-  const thema = exceptions?.thema?.value !== undefined ? exceptions.thema.value : args.thema;
+  form.preverb = null;
+  form.version = args.version;
+  form.root = args.root;
+  form.thema = args.thema;
   
-  const { person1, person2 } = select_pz_s({ root, thema }, person_s_selected);
+  form.preverbExc = exceptions.preverb;
+  form.versionExc = exceptions.version;
+  form.rootExc = exceptions.root;
+  form.themaExc = exceptions.thema;
   
-  const form = Form({ person1, version, root, thema, person2 });
-  
-  if (exceptions?.preverb) {
-    form.preverb = exceptions.preverb;
-  }
-  
-  if (exceptions?.version) {
-    form.version = exceptions.version;
-  }
-  
-  if (exceptions?.root) {
-    form.root = exceptions.root;
-  }
-  
-  if (exceptions?.thema) {
-    form.thema = exceptions.thema;
-  }
+  const stem = form.stemValue;
+  form.person1 = select_person1_s(stem, person_s);
+  form.person2 = select_person2_s(stem, person_s);
   
   return form;
 }
