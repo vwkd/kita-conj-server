6a7d22094ff1c75209afd3125a76d358411f4b1d
vwkd <33468089+vwkd@users.noreply.github.com>
2022-10-28 09:49:54 +0400

    prs: polish, export pz, add person exceptions

--- src/forms/prs.ts
+++ src/forms/prs.ts
@@ -1,6 +1,6 @@
 import { error, Form } from "./utils.ts";
 
-function select_person1_s(stem, person_s) {
+export function select_person1_s(person_s) {
   return person_s == "S1"
     ? "ვ"
     : person_s == "S2"
@@ -16,7 +16,7 @@ function select_person1_s(stem, person_s) {
     : error(`Invalid person_s "${person_s}"`);
 }
 
-function select_person2_s(stem, person_s) {
+export function select_person2_s(stem, person_s) {
   return person_s == "S1"
     ? null
     : person_s == "S2"
@@ -36,20 +36,22 @@ export default function getPRS(args, exceptions, person_s) {
   const form = Form();
   
   form.preverb = null;
+  form.person1 = select_person1_s(person_s);
   form.version = args.version;
   form.root = args.root;
   form.thema = args.thema;
   form.modus = null;
   
   form.preverbExc = exceptions.preverb;
+  form.person1Exc = exceptions.person1;
   form.versionExc = exceptions.version;
   form.rootExc = exceptions.root;
   form.themaExc = exceptions.thema;
   form.modusExc = exceptions.modus;
   
   const stem = form.stemValue;
-  form.person1 = select_person1_s(stem, person_s);
   form.person2 = select_person2_s(stem, person_s);
+  form.person2Exc = exceptions.person2;
   
   return form;
 }
