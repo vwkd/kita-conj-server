b855d646e4bfe6526c8747a13dfa57374c6c490f
vwkd <33468089+vwkd@users.noreply.github.com>
2022-10-29 07:50:23 +0400

    use data from kita-conj-parser

--- src/deps.ts
+++ src/deps.ts
@@ -1,13 +1,3 @@
 export { serve } from "https://deno.land/std@0.154.0/http/server.ts";
 export { graphql, GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLID, GraphQLNonNull, GraphQLInt, GraphQLList, GraphQLEnumType, GraphQLUnionType, GraphQLBoolean, printSchema } from "https://cdn.skypack.dev/graphql@16.6.0";
-
-export const definitions = [
-  {
-    "id": 1,
-    "preverb": "გა",
-    "version": "NEUTRAL2",
-    "root": "კეთ",
-    "thema": "ებ",
-    "exceptions": {},
-  },
-];
+export { default as definitions } from "https://raw.githubusercontent.com/vwkd/kita-conj-parser/output/vd.json" assert { type: "json" };
